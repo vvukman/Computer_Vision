40312d6573f8d894b188db2cedcaeaab33ea91e1
vvukman <vukman.vojo@gmail.com>
2016-08-31 19:30:17 -0700

    Change variable name

--- Image_Derivatives/image_derivatives.py
+++ Image_Derivatives/image_derivatives.py
@@ -7,7 +7,6 @@ def xderivative(image):
     # PIL and numpy use different internal representations
     # convert the image to a numpy array (for subsequent processing)
     image_array = np.asarray(image)
-    # Note: we need to make a copy to change the values of an array created using np.asarray
     image_array_copy = image_array.copy()
 
     xfilter = [1, -1]
@@ -30,15 +29,14 @@ def yderivative(image):
     # PIL and numpy use different internal representations
     # convert the image to a numpy array (for subsequent processing)
     image_array = np.asarray(image)
-    # Note: we need to make a copy to change the values of an array created using np.asarray
     image_array_copy = image_array.copy()
 
-    xfilter = [1, -1]
+    yfilter = [1, -1]
 
     for row in range(0, image.size[1]):
         for col in range(0, image.size[0]):
-            if(row >= math.floor(len(xfilter)/2) and row <= image.size[1] - math.floor(len(xfilter)/2) - 1):
-                delta = (image_array_copy[row, col] * xfilter[0]) + (image_array_copy[row + 1, col] * xfilter[1])
+            if(row >= math.floor(len(yfilter)/2) and row <= image.size[1] - math.floor(len(yfilter)/2) - 1):
+                delta = (image_array_copy[row, col] * yfilter[0]) + (image_array_copy[row + 1, col] * yfilter[1])
                 if(delta > 50):
                     image_array_copy[row, col] = delta
                 else:
@@ -51,9 +49,8 @@ def yderivative(image):
 
 ### Image Derivatives ###
 image = Image.open('.../perast.jpg')
-# convert the image to a black and white "luminance" greyscale image
+# convert the image to a black and white greyscale image
 image = image.convert('L')
-# convert the result back to a PIL image and save
 result = xderivative(image)
 result.save('/.../xresultm.jpg','JPEG')
 result = yderivative(image)
